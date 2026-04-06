79d5de13be2962a361cc5ec5aea37ea33a7b0524
Srihari <sanampudi.srihari2007@gmail.com>
2026-04-06 08:52:33 +0530

    Fix server entry point

--- server/app.py
+++ server/app.py
@@ -1,1 +1,12 @@
+import sys
+import os
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from main import app
+import uvicorn
+
+def main():
+    uvicorn.run("main:app", host="0.0.0.0", port=7860)
+
+if __name__ == "__main__":
+    main()
